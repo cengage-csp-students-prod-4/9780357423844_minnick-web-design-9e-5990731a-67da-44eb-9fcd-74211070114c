3f22036e4929548a5db646ab106974376ccd3481
Mikedahlin <harleybigmike@gmail.com>
2026-07-17 01:09:07 +0000

    Testing Fri Jul 17 2026 01:09:06 GMT+0000 (Coordinated Universal Time)

--- chapter10/analyze_correct_improve/student/scripts/script.js
+++ chapter10/analyze_correct_improve/student/scripts/script.js
@@ -1,12 +1,34 @@
 /*
-    Student Name:
+    Student Name: Mike Dahlin
     File Name: script.js
-    Date: 
+    Date: 07/17/26
 */
 
+//Global variables
+var figElement = document.getElementById("placeholder");
+var imgSource = document.getElementById("image");
+var figCap = document.querySelector("figcaption");
+
 //Function to display the first picture
-function pic1 {
-    imgSource.src = "images/trunk-bay.jpg"
-    imgSource.alt = Elevated view of Trunk Bay beach on St. John";
-    figElement.style.display = "block
+function pic1() {
+    imgSource.src = "images/trunk-bay.jpg";
+    imgSource.alt = "Elevated view of Trunk Bay beach on St. John";
+    figElement.style.display = "block";
     figCap.textContent = "Trunk Bay in St. John";
+}
+
+//Function to display the second picture
+function pic2() {
+    imgSource.src = "images/sanjuan.jpg";
+    imgSource.alt = "Elevated view of San Juan coast";
+    figElement.style.display = "block";
+    figCap.textContent = "Coast of San Juan";
+}
+
+//Function to display the third picture
+function pic3() {
+    imgSource.src = "images/curacao.jpg";
+    imgSource.alt = "The blue waters of Curacao";
+    figElement.style.display = "block";
+    figCap.textContent = "Curacao";
+}
